aaacd08cd5f1202ecc880c2851b78cf6b3a42000
Richard Salles <richard.danielcs@gmail.com>
2024-05-18 10:10:37 -0300

    Adicionando dialectModule para o Mysql2

--- src/core/db/database.js
+++ src/core/db/database.js
@@ -1,9 +1,8 @@
 const { Sequelize } = require('sequelize');
-const mysql2 = require('mysql2');
 
 const sequelize = new Sequelize('studio_DB', 'studio_USER', 'studio_PASSWORD', {
     dialect: 'mysql',
-    dialectModule: mysql2, // Needed to fix sequelize issues with WebPack
+    dialectModule: require('mysql2'),
     host: 'localhost',
     port: 3306,
 });
