6bb9528b2344134b83cb51054c8ee9f4304c9748
Richard Salles <richard.danielcs@gmail.com>
2024-05-26 21:57:43 -0300

    Merge

--- src/core/db/database.js
+++ src/core/db/database.js
@@ -2,16 +2,16 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-//     dialect: 'mysql',
-//     dialectModule: require('mysql2'),
-//     host: process.env.DB_HOST,
-//     port: 3306,
-// });
+ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+     dialect: 'mysql',
+     dialectModule: require('mysql2'),
+     host: process.env.DB_HOST,
+     port: 3306,
+ });
 
-const sequelize = new Sequelize('studio_DB', 'studio_USER', 'studio_PASSWORD', {
-    host: 'localhost',
-    dialect: 'mysql'
-  });
+//const sequelize = new Sequelize('studio_DB', 'studio_USER', 'studio_PASSWORD', {
+//    host: 'localhost',
+//    dialect: 'mysql'
+//  });
 
 module.exports = sequelize;
