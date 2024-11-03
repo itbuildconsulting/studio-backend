a341d0a2e35e7dedb039b8b52ae4a1ca59152b05
ItBuild Consulting <164582896+itbuildconsulting@users.noreply.github.com>
2024-11-03 10:32:43 -0300

    Update indexRoutes.ts

--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -5,7 +5,7 @@ const router = Router();
 router.get('/', (_req: Request, res: Response): Response => {
     return res.status(200).send({
         title: "Studio Backend",
-        version: "1.0.0",
+        version: "2.0.0",
         node: {
             version: "20.13.1"
         }
