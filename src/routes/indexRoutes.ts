f9dbd8bdfa053530aabf556cfe913de1acc029d6
Richard Salles <richard.danielcs@gmail.com>
2024-11-03 10:17:35 -0300

    Change Packge.json and insert vercel.json

--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response): Response => {
+router.get('/', (_req: Request, res: Response): Response => {
     return res.status(200).send({
         title: "Studio Backend",
         version: "1.0.0",
