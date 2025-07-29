84e273e1fefe30515db8510a7880db3ceea58294
Richard Salles <richard.danielcs@gmail.com>
2025-07-28 23:17:38 -0300

    FIX: Atualizando timezone

--- src/config/database.ts
+++ src/config/database.ts
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(String(process.env.DB_NAME), String(process.env.
     dialectModule: require('mysql2'),
     host: process.env.DB_HOST,
     port: 3306,
+    timezone: "-03:00", // horário de Brasília
     pool: {
         max: 20,      // Número máximo de conexões abertas
         min: 5,       // Número mínimo de conexões
